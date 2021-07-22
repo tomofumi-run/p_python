9be0d16f4693aafc807742b5f3d96d8148ef46af
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-22 21:35:11 +0900

    [p]7/22

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import random
 
 ans = random.randint(1,10)
-print("数字当てゲーム開始！", end = "")
-num = int(input())
+count = 0
 
-if ans == num:
-  print("正解です！！おめでとうございます")
-elif ans > num:
-  print("小さすぎ！")
-elif ans < num:
-  print("大きすぎ！")
-
-print("答えは %i" %ans)
+while True:
+  print("数字当てゲーム開始！", end = "")
+  num = int(input())
+  count += 1
+  if ans == num:
+    print("正解です！！おめでとうございます")
+    print("正解までのカウントは %i です" %count)
+    break
+  elif ans > num:
+    print("小さすぎ！")
+  elif ans < num:
+    print("大きすぎ！")
