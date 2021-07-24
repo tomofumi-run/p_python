ede8afbff402f592c9a73a219a3f69f25c11f646
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-24 12:54:42 +0900

    [p]7/24

---
+++ 3.py
@@ -0,0 +1,13 @@
+name = "Tomofumi"
+score = 25.52
+
+# print("name: %s, score: %-10.2f" %(name, score)) # %(型) -で左寄せ
+# name: ともふみ, score: 25.52
+
+# print("name: {0:1s},score: {1:<10.2f}".format(name,score))
+
+HERO = "アイアンマン"
+SAY = "かっこいい"
+SCORE = 120.125
+
+print("{0}は、{1}!!! 点数だと{2}ですね。".format(HERO,SAY,SCORE))
