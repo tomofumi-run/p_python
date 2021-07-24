fe101b62013fcec4932a3460bd3cd67042ca17a1
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-24 12:58:48 +0900

    [p]7/24

---
+++ 4.py
@@ -0,0 +1,11 @@
+score = int(input("数を入れてね"))
+
+
+# if score > 80:
+#   print("80点以上だね")
+# elif score > 60:
+#   print("60点以上だね")
+# else:
+#   print("まぁまぁだね")
+
+print("Niceですねぇ。" if score > 80 else "Goodですね！")
