3e4e7d400a846024e28a091d91d1afcbf131cabc
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-24 12:44:49 +0900

    [p]7/24

---
+++ 2.py
@@ -0,0 +1,19 @@
+#  + - * / // % **
+
+x = 10
+y = 20
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / 3)
+print(x // 3) # 切り捨て
+print(x % 3) # 余り
+print(x ** 2)
+
+# and or not
+
+print(True and False)
+print(True or False)
+print(not True)
+
