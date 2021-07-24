0adf30605f73c81b96833e4249b9a0635f989f30
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-24 12:39:16 +0900

    [p]7/24

--- 1.py
+++ 1.py
@@ -1,1 +1,20 @@
-print("Hello World")
+# 文字列
+voice = "声が\n遅れて\n聞こえて\nくるよ" # 改行
+html = """<div>
+私はdivに括られたマギーです。
+</div>
+"""
+
+print(voice)
+print(html)
+
+# 整数
+i = 10
+f = 23.4
+
+print(i)
+print(f)
+
+# 論理値
+bl = True
+print(bl)
